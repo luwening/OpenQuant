d48782764d4b51741d47094186e2aca20135e326
FutunnOpen <ftdev@futunn.com>
2017-03-14 10:42:56 +0800

    Update

--- 开放接口/Python接口/openft/open_quant_context.py
+++ 开放接口/Python接口/openft/open_quant_context.py
@@ -597,45 +597,6 @@ class OpenQuoteContext:
         return RET_OK, orderbook
 
 
-if __name__ == "__main__":
-
-    class StockQuoteTest(StockQuoteHandlerBase):
-        def on_recv_rsp(self, rsp_str):
-            ret_code, content = super().on_recv_rsp(rsp_str)
-            if ret_code != RET_OK:
-                print("StockQuoteTest: error, msg: %s" % content)
-                return RET_ERROR, content
-            print("StockQuoteTest ", content)
-            return RET_OK, content
-
-
-    class OrderBookTest(OrderBookHandlerBase):
-        def on_recv_rsp(self, rsp_str):
-            ret_code, content = super().on_recv_rsp(rsp_str)
-            if ret_code != RET_OK:
-                print("OrderBookTest: error, msg: %s" % content)
-                return RET_ERROR, content
-            print("OrderBookTest", content)
-            return RET_OK, content
-
-    class CurKlineTest(CurKlineHandlerBase):
-        def on_recv_rsp(self, rsp_str):
-            ret_code, content = super().on_recv_rsp(rsp_str)
-            if ret_code != RET_OK:
-                print("CurKlineTest: error, msg: %s" % content)
-                return RET_ERROR, content
-            print("CurKlineTest", content)
-            return RET_OK, content
-
-    class TickerTest(TickerHandlerBase):
-        def on_recv_rsp(self, rsp_str):
-            ret_code, content = super().on_recv_rsp(rsp_str)
-            if ret_code != RET_OK:
-                print("TickerTest: error, msg: %s" % content)
-                return RET_ERROR, content
-            print("TickerTest", content)
-            return RET_OK, content
-
 class OpenHKTradeContext:
     def __init__(self, host="127.0.0.1", sync_port=11111, async_port=11111):
         self.__host = host
